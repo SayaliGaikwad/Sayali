810f0c095ae665f0c478409817b97729ea53277d
SayaliGaikwad <sayaligaikwad80@gmail.com>
2016-07-20 04:49:15 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="./master.css">
+<link rel="stylesheet" type="text/css" href="./bootstrap.css">
+<script src="./bootstrap.js">/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}</script>
+  <title>My Coookery Class</title>
+</head>
+<body>
+<!--header-->
+<div class="container containerHead">
+<!--logo in text-->
+	<div class="col-sm-4"> <img src="./SC.png" class="img-responsive" /> </div>
+</div>
+<div class="container">
+	<nav class="navbar navbar-inverse">
+    <ul class="nav navbar-nav topnav">
+
+		<li> <a href="#" title="Home">  <span class="glyphicon glyphicon-home"></span> Home</a> </li>
+		<li> <a href="#" title="Student Corner">Student Corner</a> </li>
+		<li> <a href="#" title="Photo Gallery">Photo Gallery</a> </li>
+		<li> <a href="#" title="News Articles">News &amp; Articles</a> </li>
+		<li> <a href="#" title="Contact Us">Contact Us</a> </li>
+        <li class="icon">
+	    
+	   </li>
+	</ul>
+	</nav>
+	<div class="col-sm-3">
+	  <div class="panel panel-default">
+    	<div class="panel-heading">Recipe Categories</div>
+	    <div class="panel-body">
+            <ul>
+			<li> <a href="#" > Breads</a> </li>
+			<li> <a href="#" > Chaat </a> </li>
+			<li> <a href="#" > Desserts </a> </li>
+			<li> <a href="#" > Snacks</a> </li>
+			<li> <a href="#" > Raita</a> </li>
+			<li> <a href="#" > Rice</a> </li>
+			<li> <a href="#" > Soups and Salads</a> </li>
+			<li> <a href="#" > Vegetables Curry / Gravy</a> </li>
+			<li> <a href="#" > Sandwiches</a> </li>
+			<li> <a href="#" > Chutneys / Pickles</a> </li>
+			</ul>
+        </div>
+	  </div>
+ 	</div>
+    <div class="col-sm-9"> <img src="F2.jpg" class="img-responsive" width="100%" height="100%"/> </div>
+</div>
+	<div class="container" style="margin-top:10px;">	
+    <div class="col-sm-3">
+	  <div class="panel panel-default">
+    	<div class="panel-heading">News & Articals</div>
+	    <div class="panel-body">
+  <marquee height="250px" direction="up" scrolldelay="10" scrollamount="2" style="height: 250px;">
+            <ul id="mainul">
+				
+				<li class="newsli">
+					<a href="#" class="services-link">
+					<p class="titlenews">
+						Addmissions Open				
+					</p>
+					<p class="titlenewsdate">
+						New Batch Starting Soon...					
+					</p>
+					<p class="titlenewstext">Hurry...</p>
+					<div align="right"><span class="titlenewstext">read more &gt;&gt;</span></div>
+					</a>
+				</li>			
+			</ul>
+            <div class="spc"><br>
+            </div>
+            </marquee>
+      
+        </div>
+	  </div>
+ 	</div>
+	<div class="col-sm-3">
+	  <div class="panel panel-default">
+    	<div class="panel-heading">Quick Start Guide</div>
+	    <div class="panel-body">
+  <marquee height="250px" direction="up" scrolldelay="10" scrollamount="2" style="height: 250px;">
+            <ul id="mainul">
+				
+				<li class="newsli">
+					<a href="#" class="services-link">
+					<p class="titlenews">
+						How to use my Recipes			
+					</p>
+					<p class="titlenewsdate">
+						Essential Tips				
+					</p>
+					<p class="titlenewstext">Spices</p>
+					<div align="right"><span class="titlenewstext">read more &gt;&gt;</span>
+					</div>
+					</a>
+				</li>	
+				
+			</ul>
+            <div class="spc"><br>
+            </div>
+            </marquee>
+      
+        </div>
+	  </div>
+ 	</div>
+	<div class="col-sm-3">
+	  <div class="panel panel-default">
+    	<div class="panel-heading">Most Favorited Recipes</div>
+	    <div class="panel-body">
+  <marquee height="250px" direction="up" scrolldelay="10" scrollamount="2" style="height: 250px;">
+            <ul id="mainul">
+				
+				<li class="newsli">
+					<a href="#" class="services-link">
+					<p class="titlenews">
+						Butter Paneer Masala......
+						Aloo Paratha...
+					</p>
+					<p class="titlenewsdate">
+						Bhel Puri Chat(Spicy Crunchy Snack)
+						Dahi Vada
+					</p>
+					<p class="titlenewstext">Chola (Chana Masala) 
+					Rajma Chawal (Kidney Bean Curry)
+					</p>
+					<div align="right"><span class="titlenewstext">read more &gt;&gt;</span></div>
+					</a>
+				</li>
+						
+			</ul>
+            <div class="spc"><br>
+            </div>
+            </marquee>
+      
+        </div>
+	  </div>
+ 	</div>
+	<div class="col-sm-3">
+	  <div class="panel panel-default">
+    	<div class="panel-heading">Testimonials</div>
+	    <div class="panel-body">
+        <marquee height="250px" onmouseover="this.setAttribute(&#39;scrollamount&#39;, 0, 0);" onmouseout="this.setAttribute(&#39;scrollamount&#39;, 2, 0);" direction="up" scrolldelay="10" scrollamount="2" style="height: 250px;">
+            <ul id="mainul">
+				
+				<li class="newsli">
+					<a href="#" class="services-link">
+					<p class="titlenews">
+						Watched you on youtube, you inspired me to learn from you. 
+					</p>
+					<p class="titlenewsdate">
+						Thanks...					
+					</p>
+					<p class="titlenewstext">-Sanjiv Kapur
+					</p>
+					</a>
+				</li>
+				
+			</ul>
+            <div class="spc"><br>
+            </div>
+            </marquee>
+      
+        </div>
+	  </div>
+ 	</div>
+</div>
+<div class="container">
+	<nav class="navbar navbar-inverse">
+	<p class="footertext">Design & Developed by <a href="#"> Sayali Gaikwad </a></p>
+	</nav>
+</div>
+</body>
+</html>
